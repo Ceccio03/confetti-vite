d5732ffdb8a16084d3cf787044c92733b6101a0c
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-24 13:11:06 +0200

    commit

--- main-controller.js
+++ main-controller.js
@@ -1,4 +1,6 @@
-class MainController {
+import confetti from "canvas-confetti"
+
+export class MainController {
     constructor() {
 
     }
@@ -8,14 +10,35 @@ class MainController {
 
         const button1 = document.createElement('button');
         button1.appendChild(document.createTextNode('fire1'));
+        button1.addEventListener('click', () => {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: {y: 0.6}
+            })
+        });
         mainController.appendChild(button1);
 
         const button2 = document.createElement('button');
-        button1.appendChild(document.createTextNode('fire2'));
+        button2.appendChild(document.createTextNode('fire2'));
+        button2.addEventListener('click', () => {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: {y: 0.6}
+            })
+        });
         mainController.appendChild(button2);
 
         const button3 = document.createElement('button');
-        button1.appendChild(document.createTextNode('fire3'));
+        button3.appendChild(document.createTextNode('fire3'));
+        button3.addEventListener('click', () => {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: {y: 0.6}
+            })
+        });
         mainController.appendChild(button3);
     }
 }
